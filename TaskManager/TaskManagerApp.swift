d3f0d144abeb47593cdf7cf9b243da2045b158a3
Okello Ayai <cjokello@gmail.com>
2022-05-15 01:05:52 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Admin on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
